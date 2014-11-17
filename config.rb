b27f413c59e342269219b3476ebbf68025e01ddb
Mark Kocera <markkocera@gmail.com>
2014-11-17 17:40:56 +0100

    automatically build before deploy

--- config.rb
+++ config.rb
@@ -33,6 +33,7 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.build_before = true
   # deploy.remote   = 'origin'
   # deploy.branch   = 'gh-pages'
   # deploy.strategy = :force_push
